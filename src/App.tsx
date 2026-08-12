5849d5b0a2ec09c5b7f8de56e87e3ee2a1b7cc00
northsplash <matthewrenner@northsplash.com>
2026-08-12 12:54:25 -0400

    Add Checkout route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Portal from '@/pages/Portal';
 import Admin from '@/pages/Admin';
 import ForgotPassword from '@/pages/ForgotPassword';
 import ResetPassword from '@/pages/ResetPassword';
+import Checkout from '@/pages/Checkout';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/portal" element={<Portal />} />
+        <Route path="/checkout" element={<Checkout />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
